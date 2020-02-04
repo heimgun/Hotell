8ba0a914fbb1679042cb0c3d7585232039b48206
heimgun <heidi.guneriussen@live.no>
2020-02-04 14:00:46 +0100

    Filling up database w/values

---
+++ Floors.sql
@@ -0,0 +1,35 @@
+
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (1, 10, 0);
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (2, 10, 0);
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (3, 10, 0);
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (4, 10, 0);
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (5, 10, 0);
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (6, 10, 0);
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (7, 10, 0);
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (8, 10, 0);
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (9, 10, 0);
+
+INSERT INTO `Hotell2.1`.Floors (fl_nr, F_nrOfRooms, F_Suites)
+VALUES (10, 10, 10);
+
+SELECT * FROM `Hotell2.1`.Floors;
+
+
---
+++ Staff.sql
@@ -0,0 +1,87 @@
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (991, "Kelly Townsend", "Manager");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (981, "Jordyn Melendez", "HR");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (982, "Jensen Jacobs", "Sales");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (983, "Alfred Vega", "Sales");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (984, "Cael Pace", "Accounting");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (985, "Carter Park", "Accounting");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (971, "Lexie Frye", "Receptionist");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (972, "Brooklynn Russo", "Receptionist");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (973, "Franco Wilkinson", "Receptionist");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (974, "Marcos Freeman", "Receptionist");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (975, "Maren Strong", "Receptionist");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (961, "Tripp Acosta", "Cleaner");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (962, "Brenna Brown", "Cleaner");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (963, "Amare Francis", "Cleaner");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (964, "Marley Glover", "Cleaner");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (965, "Conor Wilson", "Cleaner");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (966, "Brianna Frederick", "Cleaner");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (967, "Eden Webb", "Cleaner");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (951, "Phoenix Pruitt", "Chef");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (952, "Kenya Finley", "Chef");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (953, "Elisa Suarez", "Chef");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (954, "Miles Cline", "Waiter");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (955, "Cara Mills", "Waiter");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (956, "Adrianna Dennis", "Waiter");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (957, "Cason Haas", "Waiter");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (958, "Cesar Ramsey", "Bartender");
+
+INSERT INTO `Hotell2.1`.Staff (S_ID, S_name, S_position)
+VALUES (959, "Savanah Mata", "Bartender");
+
+
+SELECT * FROM `Hotell2.1`.Staff;
+
+
+
